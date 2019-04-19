936a38bbe26ca7ef24b654ec016b664d0dbce68d
jordyballesteros <48108051+jordyballesteros@users.noreply.github.com>
2019-04-19 16:38:49 -0500

    Add files via upload

---
+++ data/CentrodeTransito_5.js
@@ -0,0 +1,16 @@
+var json_CentrodeTransito_5 = {
+"type": "FeatureCollection",
+"name": "CentrodeTransito_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nombre": "Salomia", "direccion": "CL 56 # 3 - 45" }, "geometry": { "type": "Point", "coordinates": [ -76.496945880892923, 3.466288690304611 ] } },
+{ "type": "Feature", "properties": { "nombre": "Sameco", "direccion": "CL 70 con AV 3B" }, "geometry": { "type": "Point", "coordinates": [ -76.5128115853624, 3.490937385356796 ] } },
+{ "type": "Feature", "properties": { "nombre": "Aventura", "direccion": "KR 100 # 15A - 61 LC 203-204" }, "geometry": { "type": "Point", "coordinates": [ -76.530134781481834, 3.368246369329296 ] } },
+{ "type": "Feature", "properties": { "nombre": "Centro Comercial Automotriz Carrera", "direccion": "CL 52 # 1B - 160" }, "geometry": { "type": "Point", "coordinates": [ -76.503523113032685, 3.472199450649631 ] } },
+{ "type": "Feature", "properties": { "nombre": "Gobernación del Valle", "direccion": "Edificio Central Gobernación" }, "geometry": { "type": "Point", "coordinates": [ -76.533444271138166, 3.449401579908701 ] } },
+{ "type": "Feature", "properties": { "nombre": "Suitco", "direccion": "KR 36 # 10 - 298 Colinas Arroyohondo. Bodega 4" }, "geometry": { "type": "Point", "coordinates": [ -76.515582358626446, 3.499545186997146 ] } },
+{ "type": "Feature", "properties": { "nombre": "Centenario", "direccion": "Centro Comercial Centenario Mezzanine P2" }, "geometry": { "type": "Point", "coordinates": [ -76.536956587546285, 3.454132880317666 ] } },
+{ "type": "Feature", "properties": { "nombre": "Parqueadero 1", "direccion": "KR 8 con CL 21" }, "geometry": { "type": "Point", "coordinates": [ -76.523396696214633, 3.451369675986329 ] } },
+{ "type": "Feature", "properties": { "nombre": "Parqueadero 2", "direccion": "DG 58 # 27 - 96 La Alborada" }, "geometry": { "type": "Point", "coordinates": [ -76.521130279296216, 3.400263908685265 ] } }
+]
+}
